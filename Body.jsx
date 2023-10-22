1b79d413f3e6ce57f493808a8fc07b1ead1c5364
THANVI M C <147711073+Thanvi-MC@users.noreply.github.com>
2023-10-22 23:06:14 +0530

    Add files via upload

---
+++ Body.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Body=() => {
+    return(
+        <>
+        <hr />
+        <h3>ABOUT ME</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nulla voluptas fuga quis, itaque vero nisi ex delectus voluptates reprehenderit eveniet facilis consectetur atque dolorum eligendi ipsam tenetur sequi numquam.
+        </p>
+        </>
+    );
+    
+
+};
+export default Body;
---
+++ Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+    return(
+        <div id="box">
+    <h1>mahatma gandhi</h1>
+    <img 
+    id= "image"
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjJa3zXKLI1Me2rtE9H3Ayp3HHso_EDUhxIw&usqp=CAU" alt="loading...." />
+    </div>
+    );
+};
+export default Header;
